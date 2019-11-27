2d1995b8c6ca25525c7779d9979e8c6f776b974d
Michael Fernandez <mikevfern@gmail.com>
2019-11-27 10:38:16 -0800

    error interceptor

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(x => {
       console.log('logged in');
     }, e => {
-      console.log('error');
+      console.log(e);
     });
   }
 
