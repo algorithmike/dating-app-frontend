b4dc115ff85efbc72aa1974cd3005f707778ae44
Michael Fernandez <mikevfern@gmail.com>
2019-12-05 10:01:24 -0800

    getUser and getUsers

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +10,19 @@ import { environment } from 'src/environments/environment';
 export class UserService {
   baseUrl = environment.apiURL;
 
-constructor() { }
+  constructor(private http: HttpClient) { }
 
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}users/${id}`, httpOptions);
+  }
 }
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + localStorage.getItem('token')
+  })
+};
